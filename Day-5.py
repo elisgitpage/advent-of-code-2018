5521127cfe423865e41e23d0374719e4e4d673d4
eabidor <eabidor@gmail.com>
2019-01-01 14:38:50 -0500

    solved day 5 part 2

--- Day-5.py
+++ Day-5.py
@@ -6,16 +6,16 @@ https://adventofcode.com/2018/day/5
 
 from string import ascii_lowercase
 
+input_data = open('input_5.txt')  # open polymer list
+pol = list(input_data.read().strip('\n'))  # read in list to string, strip \n and convert to list
+exam_pol = list('dabAcCaCBAcCcaDA')
+print(pol)
+
+
 def reacting(pols: list, i: int):
     this_letter = pols[i]
     next_letter = pols[i + 1]
     return this_letter.lower() == next_letter.lower() and this_letter != next_letter
-
-
-input_data = open('input_5.txt') # open polymer list
-pol = list(input_data.read().strip('\n')) # read in list to string, strip \n and convert to list
-#pol = list('dabAcCaCBAcCcaDA')
-print(pol)
 
 
 def react_polymers(pol):
@@ -30,22 +30,27 @@ def react_polymers(pol):
             i += 1
     return pol.__len__()
 
+
 print(react_polymers(pol))
 # PART 2
 
 pols = list(open('input_5.txt').read().strip('\n'))
 
 
-def filter_pols(listpol, unit):
-    new = listpol.copy()
-    new.remove(unit)
-    new.remove(unit.upper())
+def filter_pol_type(listpol, unit):
+    new = []
+    for p in listpol:
+        if p != unit and p != unit.upper():
+            new.append(p)
     return new
 
-
-print(['A'])
-print(filter_pols(['A'], 'a'))
-#for c in ascii_lowercase:
-#    print(c + str(react_polymers(filter_pols(pols, c))))
-
+this_len = 0
+shortest_pol = pols.__len__()
+opt_pol = ''
+for c in ascii_lowercase:
+    this_len = react_polymers(filter_pol_type(pols, c))
+    if this_len < shortest_pol:
+        shortest_pol = this_len
+        opt_pol = c
+print(f'{opt_pol:5}{shortest_pol}')
 
